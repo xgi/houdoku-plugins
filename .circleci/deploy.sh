a4be62ba1b563f13daae02e1b22b4fc2fec00d4b
Jake Robertson <jake@faltro.com>
2019-04-10 21:51:49 -0400

    avoid rerunning ci after pushing to repo

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,12 +2,12 @@ git config --global user.email "jake+xgibot@faltro.com"
 git config --global user.name "xgi-bot"
 
 git checkout -b repo
-git pull origin repo
 
 mkdir ../repo
 
 mv ./build/classes/java/main/com/faltro/houdoku/plugins/content ../repo/
 mv .git ../repo/
+mv .circleci ../repo
 
 cd ../repo
 
