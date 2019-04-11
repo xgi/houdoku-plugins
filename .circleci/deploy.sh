bf11bdf888a488618a34b572b80073d18d05f566
Jake Robertson <jake@faltro.com>
2019-04-10 22:44:47 -0400

    replace github repo with gcp bucket

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,16 +1,11 @@
-git config --global user.email "jake+xgibot@faltro.com"
-git config --global user.name "xgi-bot"
+# download google cloud sdk
+curl -o gcloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-241.0.0-linux-x86_64.tar.gz 
+tar zxvf gcloud-sdk.tar.gz google-cloud-sdk
 
-git checkout -b repo
+# authorize gcloud sdk
+echo $GCLOUD_SERVICE_KEY | ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=-
+./google-cloud-sdk/bin/gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+./google-cloud-sdk/bin/gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 
-mkdir ../repo
-
-mv ./build/classes/java/main/com/faltro/houdoku/plugins/content ../repo/
-mv .git ../repo/
-mv .circleci ../repo
-
-cd ../repo
-
-git add -A
-git commit --allow-empty -m "[auto] circleci deploy"
-git push origin repo --force
+# upload plugin classfiles to bucket
+./google-cloud-sdk/bin/gsutil cp -r build/classes/java/main/com/faltro/houdoku/plugins/content gs://houdoku-plugins
