4998fd4a420b1b2ae53b91d22300ce8bd4650d76
Jake Robertson <jake@faltro.com>
2019-04-11 15:33:53 -0400

    fix plugin classfiles upload dir

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # download/authorize google cloud sdk
 curl -o gcloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-241.0.0-linux-x86_64.tar.gz 
 tar zxvf gcloud-sdk.tar.gz google-cloud-sdk
@@ -8,6 +10,7 @@ echo $GCLOUD_SERVICE_KEY | ./gcloud-sdk/bin/gcloud auth activate-service-account
 ./gcloud-sdk/bin/gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 
 # upload plugin classfiles to bucket
+PLUGINS_DIR=build/classes/java/main/com/faltro/houdoku/plugins
 ./gcloud-sdk/bin/gsutil cp -r $PLUGINS_DIR/content gs://houdoku-plugins
 
 # build/upload index
@@ -16,6 +19,6 @@ SOURCE_DIR=src/main/java/com/faltro/houdoku/plugins
 for dir in $SOURCE_DIR/*/*; do
     dir=${dir%*/};
     cat $dir/metadata.json | jq -r
-;done | jq -sr '[.[]]' > index.json
+done | jq -sr '[.[]]' > index.json
 
 ./gcloud-sdk/bin/gsutil cp -r index.json gs://houdoku-plugins
